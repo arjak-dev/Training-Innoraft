7a5ecf387c9012f7f7eba3b29c5a1dae9487f82b
arjak-dev <arjakmondal100@gmail.com>
2020-02-08 18:24:29 +0530

    modified student

--- php_extra_assignment/student_data_storage_problem/Class_define.php
+++ php_extra_assignment/student_data_storage_problem/Class_define.php
@@ -6,9 +6,70 @@ class student {
     public $dob;
     public $grade;
     public $marks;
+    public $student_status;
 
+    function getStudentMarksDetails($student,$student_id){
+     
+      foreach($student as $key=>$value){
+        if($value->id==$student_id){
+          return $value->marks->subject_marks;
+        }
+      }
     }
+
+    function getStudentStatus($student_id,$student,$subject){
+     
+      //----------getting each student marks subject wise--------//
+      $student_marks = $this->getStudentMarksDetails($student,$student_id);
+     
+      //---------getting subject details as per the marks--------//
+      foreach($student as $key=>$value){
+        if($value->id == $student_id){
+          $subject_details = (new subject())->getSubjectDetails($subject,$value->grade);
+        break;
+        }
+      }
+     
+      //-------------calculating No. of pass Subject-----------------//
+      $pass_subject = 0;
+      for($i=0;$i<count($subject_details);$i++){
+        if($student_marks[$subject_details[$i][1]]>= $subject_details[$i][2]){
+          $pass_subject +=1;
+        } 
+      }
+    
+      
+      //-----------calculating pass subject percentage----//
+      $percentage = ($pass_subject/count($subject_details))*100;
+    
+      //------updatign the pass value of student class------//
+      if($percentage>=40){
+        foreach($student as $key=>$value){
+          if($value->id == $student_id){
+            $value->pass = "pass";
+          }
+        }
+      }
+     
+      else{
+        foreach($student as $key=>$value){
+          if($value->id == $student_id){
+            $value->pass = "fail";
+          }
+        }
+      }
+     
+        
+      foreach($student as $key=>$value){
+        if($value->id == $student_id){
+          print_r($value);
+        break;
+        }
+    }
+  }
 }
+
+
 class subject{
     
     public $name;
@@ -16,13 +77,13 @@ class subject{
     public $passing_marks;
     public $grade;
 
-    function getPassingMarksAndGrade($subject,$grade){
+    function getSubjectDetails($subject,$grade){
         $output = [];
         foreach($subject as $key=>$value){
-              if($value->grade == $grade){
-                $new =array($value->name,$value->grade,$value->passing_marks);
-                array_push($output,$new);
-               echo "<br>";
+          if($value->grade == $grade){
+              $new =array($value->name,$value->code,$value->passing_marks);
+              array_push($output,$new);
+              echo "<br>";
             }
         }
        return $output;
--- php_extra_assignment/student_data_storage_problem/data.php
+++ php_extra_assignment/student_data_storage_problem/data.php
@@ -1,87 +1,96 @@
 <?php
-   include("Class_define.php");
+  include("Class_define.php");
    
-//      $student = array();
    //-----------------------getting student data--------------------//
-   $student[0] = new student();
-   $student[0]->id = 'st1';
-   $student[0]->name= 'John';
-   $student[0]->dob =  mktime(0, 0, 0, 12, 20, 1998);
-   $student[0]->grade=12;
+  $student[0] = new student();
+  $student[0]->id = 'st1';
+  $student[0]->name= 'John';
+  $student[0]->dob =  mktime(0, 0, 0, 12, 20, 1998);
+  $student[0]->grade=12;
+  
    
-   $student[1]= new student();
-   $student[1]->id = 'st2';
-   $student[1]->name= 'Arjak';
-   $student[1]->dob = mktime(0, 0, 0, 5, 10, 1998);
-   $student[1]->grade=12;
+  $student[1]= new student();
+  $student[1]->id = 'st2';
+  $student[1]->name= 'Arjak';
+  $student[1]->dob = mktime(0, 0, 0, 5, 10, 1998);
+  $student[1]->grade=12;
 
-   $student[2]= new student();
-   $student[2]->id = 'st3';
-   $student[2]->name= 'Kunal';
-   $student[2]->dob =  mktime(0, 0, 0, 12, 20, 1998);
-   $student[2]->grade=11;
+  $student[2]= new student();
+  $student[2]->id = 'st3';
+  $student[2]->name= 'Kunal';
+  $student[2]->dob =  mktime(0, 0, 0, 12, 20, 1998);
+  $student[2]->grade=11;
    
-   $student[3]= new student();
-   $student[3]->id = 'st3';
-   $student[3]->name= 'Maity';
-   $student[3]->dob =  mktime(0, 0, 0, 12, 20, 1998);
-   $student[3]->grade= 10;
+  $student[3]= new student();
+  $student[3]->id = 'st3';
+  $student[3]->name= 'Maity';
+  $student[3]->dob =  mktime(0, 0, 0, 12, 20, 1998);
+  $student[3]->grade= 10;
    
+  $marks =[];
+  $marks[0]=new marks();
+  $marks[0]->subject_marks = ['PH'=>20,'CM'=>40,'MT'=>45];
+  $student[0]->marks = $marks[0];
+  
+  $marks[1]=new marks();
+  $marks[1]->subject_marks = ['PH'=>20,'EN'=>70];
+  $student[1]->marks = $marks[1];
 
+  $marks[2]=new marks();
+  $marks[2]->subject_marks = ['EN'=>30];
+  $student[2]->marks = $marks[2];
+
+  $marks[3]=new marks();
+  $marks[3]->subject_marks = ['MT'=>50,'CM'=>30];
+  $student[3]->marks = $marks[3];
 //    echo json_encode($student);
    //---------------------subject data input-------------------------//
    
-   $subject = [];
+  $subject = [];
 
-    $subject[0] = new subject();
-    $subject[0]->name = "physics";
-    $subject[0]->code= "PH";
-    $subject[0]->passing_marks = 30;
-    $subject[0]->grade = 12;
+  $subject[0] = new subject();
+  $subject[0]->name = "physics";
+  $subject[0]->code= "PH";
+  $subject[0]->passing_marks = 30;
+  $subject[0]->grade = 12;
 
-   $subject[1] = new subject();
-   $subject[1]->name = "Maths";
-   $subject[1]->code= "MT";
-   $subject[1]->passing_marks = 40;
-   $subject[1]->grade =10;
+  $subject[1] = new subject();
+  $subject[1]->name = "Maths";
+  $subject[1]->code= "MT";
+  $subject[1]->passing_marks = 40;
+  $subject[1]->grade =10;
 
-   $subject[2] = new subject();
-   $subject[2]->name = "English";
-   $subject[2]->code= "EN";
-   $subject[2]->passing_marks = 20;
-   $subject[2]->grade = 11;
+  $subject[2] = new subject();
+  $subject[2]->name = "English";
+  $subject[2]->code= "EN";
+  $subject[2]->passing_marks = 30;
+  $subject[2]->grade = 11;
 
-   $subject[3] = new subject();
-   $subject[3]->name = "Computer";
-   $subject[3]->code= "CM";
-   $subject[3]->passing_marks = 40;
-    $subject[3]->grade = 10;
+  $subject[3] = new subject();
+  $subject[3]->name = "Computer";
+  $subject[3]->code= "CM";
+  $subject[3]->passing_marks = 40;
+  $subject[3]->grade = 10;
 
-   $subject[4] = new subject();
-   $subject[4]->name = "English";
-   $subject[4]->code= "EN";
-   $subject[4]->passing_marks = 30;
-   $subject[4]->grade = 12;
-   (new subject())->getPassingMarksAndGrade($subject,12);
+  $subject[4] = new subject();
+  $subject[4]->name = "Maths";
+  $subject[4]->code= "MT";
+  $subject[4]->passing_marks = 30;
+  $subject[4]->grade = 12;
 
+  $subject[5] = new subject();
+  $subject[5]->name = "Computer";
+  $subject[5]->code= "CM";
+  $subject[5]->passing_marks = 30;
+  $subject[5]->grade = 12;
+
+  // print_r((new subject())->getSubjectDetails($subject,12));
+  //  print_r((new student())->getStudentMarksDetails($student,$student[0]->id)); 
+  (new student())->getStudentStatus($student[2]->id,$student,$subject);
+  //print_r($student[2]);
    
 //-------------------Marks Data Input-----------------------//
-    $marks =[];
-    $marks[0]=new marks();
-    $marks[0]->subject_marks = ['PH'=>40,'EN'=>40];
-    $student[0]->marks = $marks[0];
-    
-    $marks[1]=new marks();
-    $marks[1]->subject_marks = ['PH'=>20,'EN'=>70];
-    $student[1]->marks = $marks[1];
-
-    $marks[2]=new marks();
-    $marks[2]->subject_marks = ['EN'=>30];
-    $student[2]->marks = $marks[2];
-
-    $marks[3]=new marks();
-    $marks[3]->subject_marks = ['MT'=>50,'CM'=>30];
-    $student[3]->marks = $marks[3];
+  
 
 
    
