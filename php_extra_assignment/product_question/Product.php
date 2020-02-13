8c0ead3bc4dcbf5b9b0a9d9b78e4ee0252dc74eb
arjak-dev <arjakmondal100@gmail.com>
2020-02-13 20:50:44 +0530

    Initial commit

--- php_extra_assignment/product_question/Product.php
+++ php_extra_assignment/product_question/Product.php
@@ -1,8 +1,28 @@
 <?php
+
+  /**
+   * Defines the structure to store the products details 
+   * 
+   * @var $product_id int 
+   * Product Identity
+   * 
+   * @var $selling_date String 
+   * Selling date of the product 
+   * 
+   * @var $selling_price int
+   * Selling price of the products 
+   * 
+   * @var $total_selling price int 
+   * store the total selling price of the products as per the dates
+   * 
+   * @var $category String
+   * The category of the products   
+   */
   class Product{
     public $product_id;
     public $selling_date;
     public $selling_price;
+    public $total_selling_price;
     public $category;
 
     /**
@@ -21,9 +41,10 @@
     function __construct($product_id, $selling_dates, $selling_price, $category)
     {
       $this->product_id = $product_id;
-      $this->selling_dates = $selling_dates;
+      $this->selling_date = $selling_dates;
       $this->selling_price = $selling_price;
       $this->category = $category;
+      $this->total_selling_price = 0;
     }
 
     /**
@@ -32,8 +53,39 @@
      * @param $products Product 
      * Array contains all the product details 
      */
-    function productsort(){
-
+    function productsort($products){
+     
+      function mysort($a, $b){
+        if($a->product_id == $b->product_id){
+          return (strtotime($a->selling_date) < strtotime($b->selling_date)) ? -1 : 1;
+        }else
+        return ($a->product_id < $b->product_id) ? -1 : 1; 
+      }
+      usort($products,"mysort");
+      return $products;
     }
+
+    /**
+     * update the total selling price in the product class
+     * 
+     * @param $products Product
+     * Array hold the detaisl of the products in the accending order of the dates and the product id 
+     */
+    function totalsellingprice($products) {
+      foreach ($products as $key => $value) {
+        if (array_key_exists($key-1,$products)) {
+          if ($value->product_id == $products[$key-1]->product_id) {
+            $value->total_selling_price = $value->selling_price + $products[$key-1]->total_selling_price;
+          }else{
+            $value->total_selling_price = $value->selling_price;
+          } 
+        }else{
+          $value->total_selling_price = $value->selling_price;
+        }
+      }
+      return $products;
+    }
+
+    
 
   }
--- php_extra_assignment/product_question/index.php
+++ php_extra_assignment/product_question/index.php
@@ -1,17 +1,19 @@
 <?php
   include('Product.php');
   $array = [
-    ['pd' => 1, 'sp' => 5, 'sd' => '4thFeb', 'ct' => 'C1'],
-    ['pd' => 1, 'sp' => 15, 'sd' => '5thFeb', 'ct' => 'C1'],
-	  ['pd' => 2, 'sp' => 50, 'sd' => '4thFeb', 'ct' => 'C1'],
-	  ['pd' => 3, 'sp' => 40, 'sd' => '6thFeb', 'ct' => 'C2'],
-    ['pd' => 2, 'sp' => 75, 'sd' => '3rdFeb', 'ct' => 'C1'],
-	  ['pd' => 2, 'sp' => 65, 'sd' => '7thFeb', 'ct' => 'C1'],
-    ['pd' => 4, 'sp' => 190, 'sd' => '8thFeb', 'ct' => 'C2'],
+    ['pd' => 1, 'sp' => 5, 'sd' => '4/02/2020', 'ct' => 'C1'],
+    ['pd' => 1, 'sp' => 15, 'sd' => '5/02/2020', 'ct' => 'C1'],
+	  ['pd' => 2, 'sp' => 50, 'sd' => '4/02/2020', 'ct' => 'C1'],
+	  ['pd' => 3, 'sp' => 40, 'sd' => '6/02/2020', 'ct' => 'C2'],
+    ['pd' => 2, 'sp' => 75, 'sd' => '3/02/2020', 'ct' => 'C1'],
+	  ['pd' => 2, 'sp' => 65, 'sd' => '7/02/2020', 'ct' => 'C1'],
+    ['pd' => 4, 'sp' => 190, 'sd' => '8/02/2020', 'ct' => 'C2'],
   ];
   $i = 0;
   foreach ($array as $key => $value){
     $products[$i] = new Product($value['pd'], $value['sd'], $value['sp'], $value['ct']);
     $i += 1;
   }
-  
+ 
+  $sortedproducts = $products[0]->productsort($products);
+  print_r(json_encode($products[0]->totalsellingprice($sortedproducts)));
