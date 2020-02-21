ce9fe35a28b72e33fd40c4ccd9764d3e018174f9
arjak-dev <arjakmondal100@gmail.com>
2020-02-21 20:16:29 +0530

    added sql file

--- blog/db.sql
+++ blog/db.sql
@@ -5,7 +5,7 @@ create database blog_db;
 use blog_db;
 
 -- create user table 
-create table user (user_id int auto_incement primary key, user_name char(20),first_name char(20), last_name char(20),password char(20), email_id char(40), phone_no char(12));
+create table user (user_id int auto_increment primary key, user_name char(20),first_name char(20), last_name char(20),password char(20), email_id char(40), phone_no char(12),image char(100));
 
 -- CREATE TABLE BLOG
 create table blog(user_id int,blog_id int auto_incement primary key, blog_title char(40),blog_body text, time long, image char(30));
