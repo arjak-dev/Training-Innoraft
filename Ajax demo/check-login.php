7bfd7f98481efc6a292ff789f6ccce9ab1cab003
Arjak-dev <arjakmondal100@gmail.com>
2020-02-26 17:35:57 +0530

    learning ajax

---
+++ Ajax demo/check-login.php
@@ -0,0 +1,18 @@
+<?php
+	if(isset($_POST['name'])) {
+    echo $_POST['name']; 
+    // header("location : dashboard.html");
+  //   $password = $_POST['password'];
+  //   $wrongUsername = false;
+  //   $wrongpassword = false;
+  //   echo "abcd";
+  //   if(empty($usename) || empty($username)) {
+  //     echo "<span>please fill all the feilds </span>";
+  //     $wrongUsername = true;
+  //     $wrongpassword = true;
+  //   }
+	// } else {
+  //   echo "<span>There wan an error</span>";
+  }
+ 
+?>
