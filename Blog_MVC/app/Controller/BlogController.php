99a4bb8629b254203935e99d0f56d3d3a922b779
Arjak-dev <arjakmondal100@gmail.com>
2020-02-28 12:01:33 +0530

    class

---
+++ Blog_MVC/app/Controller/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The blog controller is user to control the different task related to a blog 
+ * like deleting blogs editing blogs etc.
+ */
+class BlogController {
+  public $BLog;
+
+  /**
+   * BlogController constructor initialize the BLog class
+   *
+   * @return void
+   */
+  function __contruct() {
+
+  }
+  /**
+   * The fucntion used to delete the blog on the user request 
+   * The owner of the blog can only delete the Blog 
+   *
+   * @return void
+   */
+  public function deleteblog() {
+
+  }
+}
