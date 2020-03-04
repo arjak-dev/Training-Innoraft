d19f2b2ae49bfe543e87dbcbc9c753eb32f20a74
Arjak-dev <arjakmondal100@gmail.com>
2020-03-04 16:09:25 +0530

    coding standards
    80 characters per line

--- Blog_MVC/app/View/login.php
+++ Blog_MVC/app/View/login.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html>
-
 <head>
   <title>
     Login to your Account
   </title>
   <link rel="icon" type="image/png" href="title_logos/icons8-toggle-on-64.png">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" 
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
   <link href='https://fonts.googleapis.com/css?family=Amiko' rel='stylesheet'>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <script 
+    src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+  </script>
+  <script 
+    src=
+    "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js">
+  </script>
+  <script 
+    src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js">
+  </script>
 </head>
-
 <body>
   <div class="bg_div">
     <div class="container center_div card bg-dark text-white">
@@ -27,10 +33,12 @@
         </a></br>
         <h5> ---OR---</h5>
         Enter user name:</br>
-        <input type="text" name="username" class="form-control" placeholder="User Name" required>
+        <input type="text" name="username" class="form-control" 
+        placeholder="User Name" required>
         <label></label></br>
         Enter password: </br>
-        <input type="password" name="password" class="form-control" placeholder="Password" required>
+        <input type="password" name="password" class="form-control" 
+        placeholder="Password" required>
         <lable>
           <?php
           if (isset($_GET['error'])) {
@@ -38,11 +46,11 @@
           }
           ?>
         </lable></br>
-        <button name="submit" type="submit" class="btn btn-primary button-position">Submit</button>
+        <button name="submit" type="submit" 
+        class="btn btn-primary button-position">Submit</button>
         <a class="btn btn-secondary button-position" href="home">CANCEL</a>
       </form>
     </div>
   </div>
 </body>
-
 </html>
