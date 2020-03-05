38426fa6bb4cd02fb30332ae0030d87a106afbed
Arjak-dev <arjakmondal100@gmail.com>
2020-03-05 11:55:35 +0530

    added comments

--- Blog_MVC/app/Controller/GoogleConfig.php
+++ Blog_MVC/app/Controller/GoogleConfig.php
@@ -2,9 +2,18 @@
   namespace Controller;
   use Google_Client;
   use Google_Service_Oauth2;
+  
+  /**
+   * GoogleConfig class handles the connection with the google client.
+   */
   class GoogleConfig {
     private $googleClient;
 
+    /**
+     * Constructor of GoogleConfig Class. It configure the Google client.
+     *
+     * @param [String] $redirect_uri the redirect uri for the google client.
+     */
     function __construct($redirect_uri){
       $this->googleClient = new Google_Client();
       $this->googleClient->setAuthConfig('resources/credentials.json');
@@ -14,14 +23,29 @@
       
     }
 
+    /**
+     * Return the auth url.
+     *
+     * @return String AuthUrl
+     */
     function createauthurl() {
       return $this->googleClient->createAuthUrl();
     }
 
+    /**
+     * Return the Google_Client Object
+     *
+     * @return Google_Client Object
+     */
     function returnclient() {
       return $this->googleClient;
     }
 
+    /**
+     * Return Google auth Object
+     *
+     * @return Google_Service_Oauth2 object
+     */
     function getserviceOauth2() {
       $objOAuthService = new Google_Service_Oauth2($this->googleClient);
       return $objOAuthService;
