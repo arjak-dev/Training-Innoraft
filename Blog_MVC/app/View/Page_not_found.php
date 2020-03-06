434f8d25ed5d8cf36b3f18071db405a29f3fceb9
Arjak-dev <arjakmondal100@gmail.com>
2020-03-06 13:33:40 +0530

    Modified the 404 page
    Added footer and header to the forever page

--- Blog_MVC/app/View/Page_not_found.php
+++ Blog_MVC/app/View/Page_not_found.php
@@ -1,24 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <style>
-    div {
-      position : absolute;
-      top: 50%;
-      left : 50%;
-      transform : translate(-50%, -50%);
-    }
-  </style>
+
+  <link rel="icon" type="image/png" href="title_logos/icons8-home-64.png">
+  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link href="css/style.css" rel="stylesheet" />
+  <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Amiko' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Autour One' rel='stylesheet'>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="css/footer.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js">
+  </script>
 </head>
+
 <body>
-  <div>
-    <img 
-      src = "https://d3e54v103j8qbb.cloudfront.net/static/page-not-found.211a85e40c.svg">
-    <h1>Page not found</h1>
-    <p>The page you are looking for is not present</p>
+  <?php include('app/View/navbar_layout.php'); ?>
+  <div style="width: 100%;
+    /* height: 500px; */
+    padding: 100px;">
+    <h1 style="color: #fff">Page not found</h1>
+    <p style="color: white; text-align: center">The page you are looking for is not present</p>
+    <a href="home">
+      <h5>Go Back To home</h5>
+    </a>
   </div>
+  <?php include('app/View/footer_layout.php'); ?>
 </body>
+
 </html>
