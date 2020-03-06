4c1a9c6cdcebf09c63d5e8dfc56247f6a47e176f
Arjak-dev <arjakmondal100@gmail.com>
2020-03-06 14:04:40 +0530

    build the navbar layout

---
+++ Blog_MVC/app/View/navbar_layout.php
@@ -0,0 +1,8 @@
+<nav class='navbar navbar-expand-mg bg-dark navbar-dark'>
+  <a class='navbar-brand logo-color' href=''>Blogify</a>
+  <ul class='display-ul'>
+    <li class='nav-item'>
+      <a class='btn btn-primary btn-sm' href='home'>Home</a>
+    </li>
+  </ul>
+</nav>
